8267b446c584a0a79523eef915fbd7011057c558
eng-oday <odaydieg@gmail.com>
2023-07-04 10:06:26 +0300

    Initial Commit

---
+++ SwiftUi-Camp/SwiftUi_CampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_CampApp.swift
+//  SwiftUi-Camp
+//
+//  Created by 3rabApp-oday on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_CampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
