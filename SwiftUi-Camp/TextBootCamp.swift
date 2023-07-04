ebe74249948b76eda83198137b2cec5d5a8b05b5
eng-oday <odaydieg@gmail.com>
2023-07-04 14:55:38 +0300

    first commit

---
+++ SwiftUi-Camp/TextBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  TextBootCamp.swift
+//  SwiftUi-Camp
+//
+//  Created by 3rabApp-oday on 04/07/2023.
+//
+
+import SwiftUI
+
+struct TextBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextBootCamp()
+    }
+}
